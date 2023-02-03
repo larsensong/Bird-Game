750e4a6cad233a44b3be41393ec19c70b338c08f
larsensong <larsenmulamba@gmail.com>
2023-02-03 23:44:14 +0300

    First Game

---
+++ Assets/Birdscript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Birdscript : MonoBehaviour
+{
+    public Rigidbody2D myRigidbody;
+    public float flapstrength;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) == true)
+        {
+            myRigidbody.velocity = Vector2.up * 10;
+        }
+        
+        
+    }
+}
